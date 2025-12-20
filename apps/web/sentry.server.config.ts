ab764027e3a776c755580289435d015fea24303c
Aaron Avila <aaronvendedor@gmail.com>
2025-12-20 14:56:49 -0400

    fix: add dynamic value for tracesSampleRate on production

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: "https://2367495471865a5179969da5070b96ca@o4510568326692864.ingest.us.sentry.io/4510568332525568",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1,
 
   // Enable logs to be sent to Sentry
   enableLogs: true,
