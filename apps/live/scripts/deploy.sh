6a5fe1f6dc7e3e7e20ae22c7d2e25c27f5e62c9f
Aaron Avila <aaronvendedor@gmail.com>
2025-12-20 11:12:24 -0400

    fix: fix deploy script

--- apps/live/scripts/deploy.sh
+++ apps/live/scripts/deploy.sh
@@ -3,4 +3,5 @@
 # We move temporally the bash script because if lk cli detects the file it will believe we are in a NodeJS project
 mv package.json package.json.tmp
 
-lk agent deploy || mv package.json.tmp package.json
+lk agent deploy
+mv package.json.tmp package.json
