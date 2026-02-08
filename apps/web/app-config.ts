c14bd2495f87f0fb219cc435155f81044711ffb3
Aaron Avila <aaronvendedor@gmail.com>
2026-02-07 21:40:28 -0400

    feat: added screen share

--- apps/web/app-config.ts
+++ apps/web/app-config.ts
@@ -26,7 +26,7 @@ export const APP_CONFIG_DEFAULTS: AppConfig = {
 
   supportsChatInput: true,
   supportsVideoInput: true,
-  supportsScreenShare: false,
+  supportsScreenShare: true,
   isPreConnectBufferEnabled: true,
 
   logo: "/lk-logo.svg",
